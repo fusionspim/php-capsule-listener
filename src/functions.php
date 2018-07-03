52f701c484fc20b853e69b436bf69806db60140f
Jamie <ziadoz@gmail.com>
2018-07-03 15:48:52 +0100

    Rename helpers. Add logging helpers.

--- src/functions.php
+++ src/functions.php
@@ -1,16 +1,36 @@
 <?php
 use FusionsPim\PhpCapsuleListener\CapsuleDebugListener;
 
-if (! function_exists('show_queries')) {
-    function show_queries(): void
+if (! function_exists('start_logging_queries')) {
+    function start_logging_queries(): void
     {
-        (new CapsuleDebugListener)->enable();
+        CapsuleDebugListener::getInstance()->enable();
     }
 }
 
-if (! function_exists('hide_queries')) {
-    function hide_queries(): void
+if (! function_exists('stop_logging_queries')) {
+    function stop_logging_queries(): void
     {
-        (new CapsuleDebugListener)->disable();
+        CapsuleDebugListener::getInstance()->disable();
+    }
+}
+
+if (! function_exists('start_capturing_queries')) {
+    function start_capturing_queries(): void
+    {
+        $listener       = CapsuleDebugListener::getInstance();
+        $listener->logs = []; // Dynamically declared so it's available in function below.
+        $listener->enable(function (array $trace) {
+            $this->logs[] = $trace;
+        });
+    }
+}
+
+if (! function_exists('stop_capturing_queries')) {
+    function stop_capturing_queries(): array
+    {
+        $listener = CapsuleDebugListener::getInstance();
+        $listener->disable();
+        return $listener->logs;
     }
 }
