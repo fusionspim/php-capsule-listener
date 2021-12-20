2f1b74b2bee88b3e54ea9e45683c73a2d3fd9cbc
Jamie <ziadoz@gmail.com>
2021-12-20 11:54:58 +0000

    Update Rector config.

--- rector.php
+++ rector.php
@@ -5,18 +5,19 @@ declare(strict_types=1);
 use Rector\Core\Configuration\Option;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/src'
-    ]);
+    // $parameters = $containerConfigurator->parameters();
+    // $parameters->set(Option::PATHS, [
+    //    __DIR__ . '/src'
+    // ]);
 
     // Define what rule sets will be applied
-    // $containerConfigurator->import(LevelSetList::UP_TO_PHP_74);
     $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
+    // $containerConfigurator->import(SetList::PHP_80);
 
     // get services (needed for register a single rule)
     // $services = $containerConfigurator->services();
